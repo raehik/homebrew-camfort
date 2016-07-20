37efebd06186662d07df22124dfe7e1ec9683106
madgen <me@madgen.net>
2016-07-20 17:25:42 +0100

    Add bottle for El Capitan & move checksum

--- Formula/camfort.rb
+++ Formula/camfort.rb
@@ -7,8 +7,9 @@ class Camfort < Formula
   desc 'Refactoring and verification tool for Fortran'
   homepage 'http://www.cl.cam.ac.uk/research/dtg/naps/'
   url 'https://hackage.haskell.org/package/camfort-0.802/camfort-0.802.tar.gz'
-  head 'https://github.com/camfort/camfort.git'
   sha256 'de057c0bfc71ff291b7a0e5581eaddaceadedc83af00774a329593a11842d6a2'
+
+  head 'https://github.com/camfort/camfort.git'
 
   depends_on 'ghc' => :build
   depends_on 'cabal-install' => :build
@@ -16,12 +17,11 @@ class Camfort < Formula
   depends_on 'pcre' => :linked
   depends_on 'gmp' => :linked
 
-  # bottle do
-  #   root_url # something
-  #   cellar :any
-  #   sha256 '' => :yosemite
-  #   sha256 'db3e662adef93ecd4639ad4101c52e20825117dcad39c804ace7ebaf6ab2e6d6' => :el_capitan
-  # end
+  bottle do
+    root_url 'http://www.cl.cam.ac.uk/research/dtg/camfort/bottles'
+    cellar :any
+    sha256 'ebe2f1cdef6d00c77eca2028013f1f5668ab2e78703ee4308b5050ca4de06b4e' => :el_capitan
+  end
 
   def install
     install_cabal_package using: %w(alex happy)
