451e9c9c6f5818d1fba6072095e55f9d22a47be4
madgen <me@madgen.net>
2016-07-21 13:28:28 +0100

    Make formula audit --strict compliant

--- Formula/camfort.rb
+++ Formula/camfort.rb
@@ -1,36 +1,36 @@
-require 'language/haskell'
+require "language/haskell"
 
 # Homebrew formula for CamFort
 class Camfort < Formula
   include Language::Haskell::Cabal
 
-  desc 'Refactoring and verification tool for Fortran'
-  homepage 'http://www.cl.cam.ac.uk/research/dtg/naps/'
-  url 'https://hackage.haskell.org/package/camfort-0.802/camfort-0.802.tar.gz'
-  sha256 'de057c0bfc71ff291b7a0e5581eaddaceadedc83af00774a329593a11842d6a2'
+  desc "Refactoring and verification tool for Fortran"
+  homepage "http://www.cl.cam.ac.uk/research/dtg/naps/"
+  url "https://hackage.haskell.org/package/camfort-0.802/camfort-0.802.tar.gz"
+  sha256 "de057c0bfc71ff291b7a0e5581eaddaceadedc83af00774a329593a11842d6a2"
 
-  head 'https://github.com/camfort/camfort.git'
+  head "https://github.com/camfort/camfort.git"
 
   bottle do
-    root_url 'http://www.cl.cam.ac.uk/research/dtg/camfort/bottles'
+    root_url "http://www.cl.cam.ac.uk/research/dtg/camfort/bottles"
     cellar :any
-    sha256 'c56debec8f1d5514d18d8c216f8197f90d06ee887fbb95f8234c1894f8a6bf90' => :yosemite
-    sha256 'ebe2f1cdef6d00c77eca2028013f1f5668ab2e78703ee4308b5050ca4de06b4e' => :el_capitan
+    sha256 "c56debec8f1d5514d18d8c216f8197f90d06ee887fbb95f8234c1894f8a6bf90" => :yosemite
+    sha256 "ebe2f1cdef6d00c77eca2028013f1f5668ab2e78703ee4308b5050ca4de06b4e" => :el_capitan
   end
 
-  depends_on 'ghc' => :build
-  depends_on 'cabal-install' => :build
-  depends_on 'gsl' => :linked
-  depends_on 'pcre' => :linked
-  depends_on 'gmp' => :linked
+  depends_on "ghc" => :build
+  depends_on "cabal-install" => :build
+  depends_on "gsl" => :linked
+  depends_on "pcre" => :linked
+  depends_on "gmp" => :linked
 
   def install
-    install_cabal_package using: %w(alex happy)
+    install_cabal_package :using => %w[alex happy]
   end
 
   test do
     # Test to see if units-suggest runs without errors
-    crit_units_test = testpath / 'crit_units_test.f90'
+    crit_units_test = testpath / "crit_units_test.f90"
     crit_units_test.write <<-EOF
       program crit_units_test
         integer x, y, z
@@ -38,6 +38,6 @@ class Camfort < Formula
       end program crit_units_test
     EOF
 
-    system bin / 'camfort', 'units-suggest', crit_units_test
+    system bin / "camfort", "units-suggest", crit_units_test
   end
 end
