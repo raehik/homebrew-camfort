c1d173b87f05e4d6f65f8ad489fedd3a2fb582a3
Mistral Contrastin <me@madgen.net>
2017-05-29 18:20:01 +0100

    0.902 update with Sierra bottle

--- Formula/camfort.rb
+++ Formula/camfort.rb
@@ -4,20 +4,19 @@ require "language/haskell"
 class Camfort < Formula
   include Language::Haskell::Cabal
 
-  VERSION = "0.900".freeze
+  VERSION = "0.902".freeze
 
   desc "Refactoring and verification tool for Fortran"
   homepage "http://www.cl.cam.ac.uk/research/dtg/naps/"
   url "https://hackage.haskell.org/package/camfort-#{VERSION}/camfort-#{VERSION}.tar.gz"
-  sha256 "fc92d5a5d5ecf42470d4f7aea2848eb785e44ba925949df86599e7b96f4a4427"
+  sha256 "3814436c2333c8be20789386c3fb18bd051fd845c8d6beadbbfd3fdf3ea9535d"
 
   head "https://github.com/camfort/camfort.git"
 
   bottle do
     root_url "https://github.com/camfort/camfort/releases/download/v#{VERSION}"
     cellar :any
-    sha256 "1c10b499fb28caea271277f54b89e70e6b28cb7bddfeaa3482e6c90e92294faf" => :yosemite
-    sha256 "12e492a28117d8f4069e95a30eaa286655b9e66561afe33d8274a0c498e6f4f0" => :el_capitan
+    sha256 "593c11b580dbd8cfc1e1ddaaa582434afdb58760d813619bddcd27bdbe91d6b8" => :sierra
   end
 
   depends_on "ghc" => :build
@@ -25,6 +24,7 @@ class Camfort < Formula
   depends_on "gsl" => :linked
   depends_on "pcre" => :linked
   depends_on "gmp" => :linked
+  depends_on "z3" => :linked
 
   def install
     install_cabal_package :using => %w[alex happy]
