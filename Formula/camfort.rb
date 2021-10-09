937da1efd1daa93032b134a4f75c642742a2b8af
Ben Orchard <thefirstmuffinman@gmail.com>
2021-10-09 14:52:44 +0100

    update CamFort version
    Closes #3.
    
    Signed-off-by: Ben Orchard <thefirstmuffinman@gmail.com>

--- Formula/camfort.rb
+++ Formula/camfort.rb
@@ -1,10 +1,10 @@
 class Camfort < Formula
-  VERSION = "1.1.1".freeze
+  VERSION = "1.1.2".freeze
 
   desc "Refactoring and verification tool for Fortran"
   homepage "https://camfort.github.io"
   url "https://hackage.haskell.org/package/camfort-#{VERSION}/camfort-#{VERSION}.tar.gz"
-  sha256 "4e18389bdaa075d499383dde88c1ccb00f2e695c05622447296a112e5c2d739c"
+  sha256 "d2b16259b486faf90c4a6144ae19ff94e02bd69d5e97773effb1244724e9468d"
   license "Apache-2.0"
   head "https://github.com/camfort/camfort.git", branch: "master"
 
